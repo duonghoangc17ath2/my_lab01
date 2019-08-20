f2999bd506c6ad467e68425e6bfaf70252898198
w7 <w7@PC230>
2019-08-20 16:52:47 +0700

    test 1

--- src/bt2/frm_congtrunhanchia.java
+++ src/bt2/frm_congtrunhanchia.java
@@ -235,6 +235,8 @@ public class frm_congtrunhanchia extends javax.swing.JFrame {
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
         this.dispose();
+        
+        
     }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
